8c23966f7b98d43ed753d8da92b5203d837e185e
Prakash Bhandari <pbhandari@lftechnology.com>
2013-06-06 21:19:58 +0545

    adding git hub

---
+++ config_n.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of Osclass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for Osclass */
+define('DB_NAME', 'osclass');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'Nohyun83');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'oc1_');
+
+define('REL_WEB_URL', '/');
+
+define('WEB_PATH', 'http://66.175.209.111/');
+
+?>
